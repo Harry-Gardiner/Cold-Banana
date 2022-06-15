cc6f7e24037ba8b80ad528343f24a6a87246d8f7
Harry-Gardiner <gardiner_harry@yahoo.com>
2022-06-15 16:44:25 +0100

    Products - grid layout controlled by css grid

--- src/components/ProductsGrid.vue
+++ src/components/ProductsGrid.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="products-grid container d-flex flex-wrap">
+  <section class="products-grid container">
     <div class="product">P1</div>
     <div class="product">P2</div>
     <div class="product">P3</div>
@@ -28,5 +28,8 @@ export default {
 
 <style lang="scss" scoped>
 .products-grid {
+  display: grid;
+  gap: 1rem;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 33%));
 }
 </style>
