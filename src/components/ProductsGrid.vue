e127704520038872965073653510ab666a3929a3
Harry-Gardiner <gardiner_harry@yahoo.com>
2022-06-15 16:15:18 +0100

    Products - created products component with base content for testing and layout

---
+++ src/components/ProductsGrid.vue
@@ -0,0 +1,32 @@
+<template>
+  <section class="products-grid container d-flex flex-wrap">
+    <div class="product">P1</div>
+    <div class="product">P2</div>
+    <div class="product">P3</div>
+    <div class="product">P4</div>
+    <div class="product">P5</div>
+    <div class="product">P6</div>
+  </section>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "ProductsGrid",
+  components: {},
+  data() {
+    return {
+      data: null,
+    };
+  },
+  mounted() {
+    axios.get(this.apiUrl).then((response) => (this.data = response));
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.products-grid {
+}
+</style>
