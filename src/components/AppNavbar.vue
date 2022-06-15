1cfc27ce0aefcaa35a9c904a24638d85151f53a2
Harry-Gardiner <gardiner_harry@yahoo.com>
2022-06-15 09:26:01 +0100

    Navbar - added styling for mobile menu

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -4,20 +4,22 @@
       <h1>Chilled Grape</h1>
     </div>
     <ul class="navabr__navigation d-none d-md-flex">
-      <li><a href="/" class="navbar__navigation__link">Home</a></li>
-      <li><a href="/" class="navbar__navigation__link">About</a></li>
-      <li><a href="/" class="navbar__navigation__link">Blog</a></li>
-      <li><a href="/" class="navbar__navigation__link">Pricing</a></li>
+      <li><a href="/" class="navbar__link">Home</a></li>
+      <li><a href="/" class="navbar__link">About</a></li>
+      <li><a href="/" class="navbar__link">Blog</a></li>
+      <li><a href="/" class="navbar__link">Pricing</a></li>
     </ul>
     <div class="navbar__icon d-flex d-md-none">
       <i @click="toggleMobileNav" class="fa fa-bars"></i>
     </div>
-    <ul v-show="mobileNav" class="navbar__mobile-nav d-md-none">
-      <li><a href="/" class="navbar__navigation__link">Home</a></li>
-      <li><a href="/" class="navbar__navigation__link">About</a></li>
-      <li><a href="/" class="navbar__navigation__link">Blog</a></li>
-      <li><a href="/" class="navbar__navigation__link">Pricing</a></li>
-    </ul>
+    <transition name="mobile-nav">
+      <ul v-show="mobileNav" class="navbar__mobile-nav d-md-none">
+        <li><a href="/" class="navbar__link">Home</a></li>
+        <li><a href="/" class="navbar__link">About</a></li>
+        <li><a href="/" class="navbar__link">Blog</a></li>
+        <li><a href="/" class="navbar__link">Pricing</a></li>
+      </ul>
+    </transition>
   </nav>
 </template>
 
@@ -38,4 +40,69 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.navbar {
+  position: sticky;
+  z-index: 99;
+  width: 100%;
+  top: 0;
+  transition: 0.5s ease all;
+  color: $heading-colour;
+
+  &__branding {
+    text-transform: uppercase;
+  }
+
+  ul,
+  &__link {
+    font-weight: 500;
+    list-style: none;
+    text-decoration: none;
+  }
+
+  &__link {
+    font-size: 14px;
+    transition: 0.5s ease all;
+    padding-bottom: 2px;
+    border-bottom: 2px solid transparent;
+
+    &:hover {
+      border-color: purple;
+    }
+  }
+
+  &__mobile-nav {
+    display: flex;
+    flex-direction: column;
+    position: fixed;
+    width: 100%;
+    max-width: 250px;
+    height: 100%;
+    background: $prim-colour;
+    top: 0;
+    left: 0;
+    padding-top: 2rem;
+
+    li {
+      margin: 1rem 0;
+
+      a {
+        color: $alt-text-colour;
+      }
+    }
+  }
+
+  .mobile-nav-enter-active,
+  .mobile-nav-leave-active {
+    transition: 1s ease all;
+  }
+
+  .mobile-nav-enter-from,
+  .mobile-nav-leave-to {
+    transform: translateX(-250px);
+  }
+
+  .mobile-nave-enter-to {
+    transform: translateX(0);
+  }
+}
 </style>
