0c7bee78239311797652a1eaabb4d9a732a0b87e
Harry-Gardiner <gardiner_harry@yahoo.com>
2022-06-15 15:09:57 +0100

    Image and Text - created component with base content

---
+++ src/components/ImageWithText.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="text-and-image container">
+    <div class="row">
+      <div class="col-md-6">
+        <img src="" alt="" />
+      </div>
+      <div class="col-md-6">
+        <h2>{{ title }}</h2>
+        <p>{{ content }}</p>
+        <AppButton>Read More</AppButton>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import AppButton from "./AppButton.vue";
+export default {
+  name: "ImageWithText",
+  components: {
+    AppButton,
+  },
+  props: {
+    title: String,
+    content: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
