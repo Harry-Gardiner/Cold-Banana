ca9e8b52253e83d416ef2c6b036397483658aa74
Harry-Gardiner <gardiner_harry@yahoo.com>
2022-06-15 09:03:33 +0100

    Navbar - added mobile menu toggle

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -3,16 +3,16 @@
     <div class="navbar__branding">
       <h1>Chilled Grape</h1>
     </div>
-    <ul class="navabr__navigation">
+    <ul class="navabr__navigation d-none d-md-flex">
       <li><a href="/" class="navbar__navigation__link">Home</a></li>
       <li><a href="/" class="navbar__navigation__link">About</a></li>
       <li><a href="/" class="navbar__navigation__link">Blog</a></li>
       <li><a href="/" class="navbar__navigation__link">Pricing</a></li>
     </ul>
-    <div class="navbar__icon">
-      <i class="fa fa-bars"></i>
+    <div class="navbar__icon d-flex d-md-none">
+      <i @click="toggleMobileNav" class="fa fa-bars"></i>
     </div>
-    <ul class="navbar__mobile-nav">
+    <ul v-show="mobileNav" class="navbar__mobile-nav d-md-none">
       <li><a href="/" class="navbar__navigation__link">Home</a></li>
       <li><a href="/" class="navbar__navigation__link">About</a></li>
       <li><a href="/" class="navbar__navigation__link">Blog</a></li>
@@ -24,6 +24,16 @@
 <script>
 export default {
   name: "AppNavbar",
+  data() {
+    return {
+      mobileNav: false,
+    };
+  },
+  methods: {
+    toggleMobileNav() {
+      this.mobileNav = !this.mobileNav;
+    },
+  },
 };
 </script>
 
